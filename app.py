f6e17d98a1be15220641485311eb2aba2aa22a25
dlanx6 <144339211+dlanx6@users.noreply.github.com>
2024-08-21 17:40:53 +0800

    Added base URL for PokeAPI

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import requests
 from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
-@app.route('/')
+
+base_url = "https://pokeapi.co/api/v2/"
+
+
+@app.route("/")
 def index():
     return render_template("index.html")
