bb81e16fecb0cff30a1f24bcacdef3a26ac76b87
dlanx6 <144339211+dlanx6@users.noreply.github.com>
2024-08-26 19:31:34 +0800

    Added pokemon colors and dynamic type styles, also added animation for stats bar

--- static/main.js
+++ static/main.js
@@ -1,4 +1,15 @@
-// Timeout for flash messages
-setTimeout(function() {
-    document.querySelector('.flash-message').style.display = 'none';
-}, 3000);
+document.addEventListener('DOMContentLoaded', () => {
+    // Timeout for flash messages
+    setTimeout(() => {
+        document.querySelector('.flash-message').style.display = 'none';
+    }, 3000);
+
+    // Pokemon types
+    const types = document.querySelectorAll('.pokemon-type');
+
+    // Add colors based on the Pokemon types
+    types.forEach(type => {
+        let text = type.textContent.toLowerCase();
+        type.classList.add(text);
+    });
+});
